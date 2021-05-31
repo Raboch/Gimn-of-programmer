4d07b855b60c2a4c5fdc7cda8c1543a4973d2a8c
Andr <amsh23453@gmail.com>
2021-05-31 20:16:49 +0700

    Добавьте файлы проекта.

---
+++ Gimn of programmer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimn_of_programmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здраствуйте Алексей;");
+            Console.WriteLine("Этот стих для Вас");            
+            Console.WriteLine("Вы мышление программиста подорили нам;");
+            Console.WriteLine("И идеи подорили нам;");
+            Console.WriteLine("И этот стих пишет для Вас ваш Андрей;");
+            Console.WriteLine("Теперь как жить без Вас?;");
+            Console.WriteLine(" ");
+            Console.WriteLine("Вы говорили что вы ленивы(ведь прогроммисты ленивы);");
+            Console.WriteLine("Но от лени Вы нас спасли;");
+            Console.WriteLine("Замотивировали нас, берегли;");
+            Console.WriteLine("Вы не ленивы;");
+            Console.WriteLine("Вы Выростили нас;");
+            Console.WriteLine("Вы не просто учитель для нас...;");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Вы наш опекун...;");
+
+            Console.ReadKey();
+        }
+    }
+}
